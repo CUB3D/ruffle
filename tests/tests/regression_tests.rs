604d874a2c47ce335ab79bde058ad88cf2ad1857
CUB3D <callumthom11@gmail.com>
2022-08-09 22:41:42 +0100

    avm1: Filereference.upload tests

--- tests/tests/regression_tests.rs
+++ tests/tests/regression_tests.rs
@@ -610,6 +610,8 @@ swf_tests! {
     (file_reference_download_success, "avm1/file_reference_download_success", 1),
     (file_reference_download_cancel, "avm1/file_reference_download_cancel", 1),
     (file_reference_download_httperror, "avm1/file_reference_download_httperror", 1),
+    (file_reference_upload_success, "avm1/file_reference_upload_success", 1),
+    (file_reference_upload_httperror, "avm1/file_reference_upload_httperror", 1),
     (displacement_map_filter, "avm1/displacement_map_filter", 1),
     (divide_swf4, "avm1/divide_swf4", 1),
     (do_init_action, "avm1/do_init_action", 3),
